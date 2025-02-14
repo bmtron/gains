a470134feaff28e27c4a3a57e03798262427331a
Brendan Meehan <brendan@brendanmeehan.com>
2025-02-13 21:56:39 -0500

    internal server address update

--- constants/serveraddress.ts
+++ constants/serveraddress.ts
@@ -1,4 +1,4 @@
-const serverAddress = "http://192.168.50.46:5000";
+const serverAddress = "http://192.168.50.43:5000";
 const serverAddressWebDebug = "http://127.0.0.1:5214";
 const serverAddressDebug = "http://192.168.50.231:5214";
 
